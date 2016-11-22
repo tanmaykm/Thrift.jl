764571950d57bbbb69056553dd0227d4b36402a9
tan <tanmaykm@gmail.com>
2016-11-22 12:35:48 +0530

    fix Thrift build using BinDeps
    ref #21
    fixes #20
    
    - cloning Thrift git repo for sources
    - using automake to generate makefiles because we patch Makefile.am for Julia (needs automake to be installed already)
    - setting `pkgdir/deps` as install prefix of all Thrift language plugins (to avoid needing `sudo`)

--- test/gen.jl
+++ test/gen.jl
@@ -1,9 +1,12 @@
 testdir = dirname(@__FILE__)
-ENV["PATH"] = join([joinpath(dirname(@__FILE__), "deps"), ENV["PATH"]], ":")
+ENV["PATH"] = join([joinpath(dirname(@__FILE__), "deps", "bin"), ENV["PATH"]], is_windows() ? ";" : ":")
 
-run(`thrift -gen jl srvcctrl.thrift`)
-run(`thrift -gen jl proto_tests.thrift`)
+const RUN_ENV = isless(Base.VERSION, v"0.5.0-") ? ["$n=$v" for (n,v) in ENV] : ENV
+
+run(Cmd(`thrift -gen jl srvcctrl.thrift`, env=RUN_ENV))
+run(Cmd(`thrift -gen jl proto_tests.thrift`, env=RUN_ENV))
 println("Compiled IDLs...")
+
 cp(joinpath(testdir, "srvcctrl_impl.jl"), joinpath(testdir, "gen-jl", "srvcctrl", "srvcctrl_impl.jl"); remove_destination=true)
 cp(joinpath(testdir, "proto_tests_impl.jl"), joinpath(testdir, "gen-jl", "proto_tests", "proto_tests_impl.jl"); remove_destination=true)
 println("Added service implementations...")
