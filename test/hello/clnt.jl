b918e5e46db69a797873bfe37d5c5eeef4268537
Martijn Visser <mgvisser@gmail.com>
2020-02-16 18:25:58 +0100

    fix hello example import
    ```
    ~/.julia/dev/Thrift/test/hello$ julia srvr.jl
    ERROR: LoadError: ArgumentError: Package hello not found in current path:
    - Run `import Pkg; Pkg.add("hello")` to install the hello package.
    ```

--- test/hello/clnt.jl
+++ test/hello/clnt.jl
@@ -3,17 +3,16 @@ import Thrift.process, Thrift.meta
 
 # include the generated hello module
 include("gen-jl/hello/hello.jl");
-import hello.SayHelloClient, hello.hello_to
 
 # create a client instance with our choice of protocol and transport
 clnt_transport = TSocket(19999)
 proto = TBinaryProtocol(clnt_transport)
-clnt = SayHelloClient(proto)
+clnt = hello.SayHelloClient(proto)
 
 # open a connection
 open(clnt_transport)
 # invoke service and print the result
-println(hello_to(clnt, "Julia"))
+println(hello.hello_to(clnt, "Julia"))
 # close connection
 close(clnt_transport)
 
