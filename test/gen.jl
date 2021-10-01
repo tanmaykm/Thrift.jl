17f5914c096d06730e828fce6abbe568f50566c8
tan <tanmaykm@gmail.com>
2021-10-01 07:22:55 +0530

    disambiguate service method names
    This, along with the Thrift compiler change done in https://github.com/JuliaBinaryWrappers/ThriftJuliaCompiler_jll.jl/releases/tag/ThriftJuliaCompiler-v0.12.1%2B0 allows defining multiple services in a thrift schema to have overlapping method names.
    
    fixes #64

--- test/gen.jl
+++ test/gen.jl
@@ -6,18 +6,26 @@ const testdir = dirname(@__FILE__)
 
 function generate()
     @testset "code generation" begin
-        # generate code
-        for (proto_name,type_name) in (("srvcctrl","ServiceControl"), ("proto_tests","ProtoTests"))
-            Thrift.generate(proto_name * ".thrift")
-            for suffix in ("_constants.jl", "_types.jl", ".jl")
-                @test isfile(joinpath(testdir, "gen-jl", proto_name, proto_name * suffix))
+        @testset "basic" begin
+            # generate code
+            for (proto_name,type_name) in (("srvcctrl","ServiceControl"), ("proto_tests","ProtoTests"))
+                Thrift.generate(proto_name * ".thrift")
+                for suffix in ("_constants.jl", "_types.jl", ".jl")
+                    @test isfile(joinpath(testdir, "gen-jl", proto_name, proto_name * suffix))
+                end
+                @test isfile(joinpath(testdir, "gen-jl", proto_name, type_name * ".jl"))
             end
-            @test isfile(joinpath(testdir, "gen-jl", proto_name, type_name * ".jl"))
-        end
 
-        # add service implementations
-        cp(joinpath(testdir, "srvcctrl_impl.jl"), joinpath(testdir, "gen-jl", "srvcctrl", "srvcctrl_impl.jl"); force=true)
-        cp(joinpath(testdir, "proto_tests_impl.jl"), joinpath(testdir, "gen-jl", "proto_tests", "proto_tests_impl.jl"); force=true)
+            # add service implementations
+            cp(joinpath(testdir, "srvcctrl_impl.jl"), joinpath(testdir, "gen-jl", "srvcctrl", "srvcctrl_impl.jl"); force=true)
+            cp(joinpath(testdir, "proto_tests_impl.jl"), joinpath(testdir, "gen-jl", "proto_tests", "proto_tests_impl.jl"); force=true)
+        end
+        @testset "service method disambiguation" begin
+            Thrift.generate("disambiguate_svc_method.thrift")
+            touch(joinpath(testdir, "gen-jl", "disambiguate_svc_method", "disambiguate_svc_method_impl.jl"))
+            include(joinpath(testdir, "gen-jl", "disambiguate_svc_method", "disambiguate_svc_method.jl"))
+            @test disambiguate_svc_method.hello_to_args_Other !== disambiguate_svc_method.hello_to_args_SayHello
+        end
     end
 end
 
