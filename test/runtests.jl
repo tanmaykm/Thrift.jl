93dea5830c202d1e48f2cd9d3fc3e9a838dca81e
Tom Kwong <tk3369@gmail.com>
2021-11-07 01:04:55 -0700

    Add headertransport_tests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,6 +12,7 @@ else
         @info("Running protocol and transport tests")
         include("memtransport_tests.jl")
         include("filetransport_tests.jl")
+        include("headertransport_tests.jl")
         include("utils_tests.jl")
     end
 end
